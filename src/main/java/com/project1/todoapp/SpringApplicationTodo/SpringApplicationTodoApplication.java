53789ee9835de66d91258eabd92d7d559f2317b7
saikumarvaddepally15 <vsai0304@gmail.com>
2024-05-16 17:07:49 -0400

    First commit

---
+++ src/main/java/com/project1/todoapp/SpringApplicationTodo/SpringApplicationTodoApplication.java
@@ -0,0 +1,13 @@
+package com.project1.todoapp.SpringApplicationTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project1/todoapp/SpringApplicationTodo/SpringApplicationTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.todoapp.SpringApplicationTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
